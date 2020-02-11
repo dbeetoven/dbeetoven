156807b6fcfc93ec7792fa3261ab83e000849e29
Beetoven Desir <beetovenson@gmail.cim>
2020-02-11 20:10:08 -0300

    Updates

---
+++ precache-manifest.9b4337c9b21ddb731e907d1f0d86e9c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228f7462ad2dc257f718048f8f452fc7",
+    "url": "/dbeetoven/index.html"
+  },
+  {
+    "revision": "4e80b8025625afcc0641",
+    "url": "/dbeetoven/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "89b9356952c209c63cdc",
+    "url": "/dbeetoven/static/js/2.25a3054d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dbeetoven/static/js/2.25a3054d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e80b8025625afcc0641",
+    "url": "/dbeetoven/static/js/main.389b7b74.chunk.js"
+  },
+  {
+    "revision": "e5d8827bf0537c9fa012",
+    "url": "/dbeetoven/static/js/runtime-main.2746658f.js"
+  },
+  {
+    "revision": "f38c752fcfbc26bca3ae2a21e4107fdc",
+    "url": "/dbeetoven/static/media/profile.f38c752f.jpg"
+  }
+]);
