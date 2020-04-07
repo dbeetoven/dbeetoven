1989a03f93d4d8b5082ca2c0b9c5b2d499186b72
dbeetoven <beetovenson@gmail.com>
2020-04-07 03:01:41 -0300

    Updates

---
+++ precache-manifest.0e9de023e0df13922113d1a31ff9e39f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f118e48bf37129428ccda4280c390ff",
+    "url": "/dbeetoven/index.html"
+  },
+  {
+    "revision": "1e355bd5a3238efcffad",
+    "url": "/dbeetoven/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "53d4164499c9a736b37d",
+    "url": "/dbeetoven/static/js/2.36059290.chunk.js"
+  },
+  {
+    "revision": "1e355bd5a3238efcffad",
+    "url": "/dbeetoven/static/js/main.e1868ebb.chunk.js"
+  },
+  {
+    "revision": "bf7dd92abc54d9a92bc3",
+    "url": "/dbeetoven/static/js/runtime~main.340b209b.js"
+  },
+  {
+    "revision": "f38c752fcfbc26bca3ae2a21e4107fdc",
+    "url": "/dbeetoven/static/media/profile.f38c752f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dbeetoven/precache-manifest.9164103c73faaba8d5c5361262779868.js"
+  "/dbeetoven/precache-manifest.0e9de023e0df13922113d1a31ff9e39f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dbeetoven/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
